0e4b45ea8c23bfb522a68862e3af7a852edaa9b3
Milan Werner <milan.werner@student.howest.be>
2026-02-26 15:10:03 +0100

    Initial commit

---
+++ Minigin/ResourceManager.cpp
@@ -0,0 +1,54 @@
+﻿#include <stdexcept>
+#include <SDL3_ttf/SDL_ttf.h>
+#include "ResourceManager.h"
+#include "Renderer.h"
+#include "Texture2D.h"
+#include "Font.h"
+
+namespace fs = std::filesystem;
+
+void dae::ResourceManager::Init(const std::filesystem::path& dataPath)
+{
+	m_dataPath = dataPath;
+
+	if (!TTF_Init())
+	{
+		throw std::runtime_error(std::string("Failed to load support for fonts: ") + SDL_GetError());
+	}
+}
+
+void dae::ResourceManager::Destroy()
+{
+	m_loadedFonts.clear();
+	m_loadedTextures.clear();
+	TTF_Quit();
+}
+
+std::shared_ptr<dae::Texture2D> dae::ResourceManager::LoadTexture(const std::string& file)
+{
+	const auto fullPath = m_dataPath/file;
+	const auto filename = fs::path(fullPath).filename().string();
+	if(m_loadedTextures.find(filename) == m_loadedTextures.end())
+		m_loadedTextures.insert(std::pair(filename,std::make_shared<Texture2D>(fullPath.string())));
+	return m_loadedTextures.at(filename);
+}
+
+std::shared_ptr<dae::Font> dae::ResourceManager::LoadFont(const std::string& file, uint8_t size)
+{
+	const auto fullPath = m_dataPath/file;
+	const auto filename = fs::path(fullPath).filename().string();
+	const auto key = std::pair<std::string, uint8_t>(filename, size);
+	if(m_loadedFonts.find(key) == m_loadedFonts.end())
+		m_loadedFonts.insert(std::pair(key,std::make_shared<Font>(fullPath.string(), size)));
+	return m_loadedFonts.at(key);
+}
+
+void dae::ResourceManager::UnloadUnusedResources()
+{
+	std::erase_if(m_loadedTextures, [](const auto& item) {
+	    return item.second.use_count() == 1;
+	});
+	std::erase_if(m_loadedFonts, [](const auto& item) {
+	    return item.second.use_count() == 1;
+	});
+}
